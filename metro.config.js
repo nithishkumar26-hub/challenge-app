9b3a0c17ef0e5e7074d1fd3171a36b8b36ed196a
NithishKumar-Trata <nithishkumar2681@gmail.com>
2025-06-28 16:36:22 +0530

    Fix Expo Port Configuration

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,9 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Force Metro to use port 8081
+config.server = {
+  port: 8081,
+};
+
 module.exports = config;
